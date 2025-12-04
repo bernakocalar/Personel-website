4baba70a3168bae88168ab020dd617745b71dac4
bernakocalar <berna.kocalar@gmail.com>
2025-12-04 17:28:27 +0300

    projects added

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Home from "./pages/HomePage";
 import Series from "./pages/Series";
 import Movies from "./pages/Movies";
 import Navbar from "./components/Navbar";
+import Portfolyo from "./pages/Portfolyo";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/series" element={<Series />} />,
         <Route path="/movies" element={<Movies />} />
+        <Route path="/works" element={<Portfolyo />} />
       </Routes>
     </>
   );
